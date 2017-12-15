36bbb2eb045f2260f5549b7a7887f7559fd990d4
Justin Wendlandt <jwendl@hotmail.com>
2017-12-14 17:58:04 -0800

    Got simulation data to work between sf applications.

---
+++ DeviceSimulation/DeviceSimulator/Extensions/DoubleExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Bogus;
+
+namespace DeviceSimulator.Extensions
+{
+    public static class DoubleExtensions
+    {
+        public static double TweakValue(this double baseValue, double maxRange)
+        {
+            var randomizer = new Randomizer();
+            var oddEven = randomizer.Int() % 2 == 0;
+            var delta = randomizer.Double(0, maxRange);
+            if (oddEven)
+            {
+                baseValue += delta;
+            }
+            else
+            {
+                baseValue -= delta;
+            }
+
+            return baseValue;
+        }
+    }
+}
---
+++ DeviceSimulation/DeviceSimulator/Models/TruckData.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace DeviceSimulator.Models
+{
+    public class TruckData
+        : DeviceData
+    {
+        [JsonProperty("latitude")]
+        public double Latitude { get; set; }
+
+        [JsonProperty("longitude")]
+        public double Longitude { get; set; }
+    }
+}
