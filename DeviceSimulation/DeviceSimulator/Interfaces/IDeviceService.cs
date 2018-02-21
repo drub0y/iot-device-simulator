94f2d2cde1d8b0f37e4aab93d9a474dbdc738b6c
Justin Wendlandt <jwendl@hotmail.com>
2018-02-21 11:08:05 -0800

    Fixing bugs on simple example....

--- DeviceSimulation/DeviceSimulator/Interfaces/IDeviceService.cs
+++ DeviceSimulation/DeviceSimulator/Interfaces/IDeviceService.cs
@@ -1,6 +1,10 @@
-﻿namespace DeviceSimulator.Interfaces
+﻿using System.Threading.Tasks;
+
+namespace DeviceSimulator.Interfaces
 {
     public interface IDeviceService
     {
+        Task ConnectAsync();
+        Task SendEventAsync<T>(T item);
     }
 }
